6b7daf61b090e4a2d998feb0fd3e370400d0ca9e
Hasan Ali <kk631352@gmail.com>
2021-03-24 20:55:59 +0530

    Added a new variable

--- Index.java
+++ Index.java
@@ -1,7 +1,8 @@
-public class Index{
+public class Index {
   public static void main(String[] args) {
     int a = 10;
     int b = 2;
-    System.out.println(a+b);
+    int c = 3;
+    System.out.println(a + b + c);
   }
 }
